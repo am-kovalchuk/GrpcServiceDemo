71bd5db4db8c20ba00612243e3a0ec6fe1b4529d
Andrey Kovalchuk <amkovalchuk@outlook.com>
2024-12-31 11:39:04 -0600

    Add project files.

---
+++ GrpcService/Services/PaymentsServices.cs
@@ -0,0 +1,52 @@
+﻿using Grpc.Core;
+using GrpcService.Protos;
+
+namespace GrpcService.Services
+{
+    public class PaymentsServices : Payment.PaymentBase
+    {
+        private readonly ILogger<PaymentsServices> _logger;
+
+        public PaymentsServices(ILogger<PaymentsServices> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<PaymentVerificationResponse> GetOrderPayment(OrderPaymentModel request, ServerCallContext context)
+        {
+            PaymentVerificationResponse response = new PaymentVerificationResponse();
+
+            if (request != null && request?.OrderNumber != null)
+            {
+                if (request.CreditCardNumber != null)
+                {
+                    //verify credid card number is 16 digits
+                    int lengthCC = request.CreditCardNumber.Length;
+                    int lengthCVC = request.CreditCardSecurityCode.Length;
+                    response.OrderNumber = request.OrderNumber;
+
+                    if (lengthCC == 16 && lengthCVC == 3 && request?.Amount != null)
+                    {
+                        response.Success = true;
+                        response.Message = "The payment is Successful.";
+                        return Task.FromResult(response);
+                    }
+                    else
+                    {
+                        response.Success = false;
+                        response.Message = "Invalid Credit Card. Please Try Again";
+                        return Task.FromResult(response);
+                    }
+                }
+                else
+                {
+                    response.Success = false;
+                    response.Message = "No Payment Method specified.";
+                    return Task.FromResult(response);
+                }
+            }
+
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ OrderClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Net.Client;
+using GrpcService.Protos;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7065");
+var paymentsClient = new Payment.PaymentClient(channel);
+
+
+var orderPaymentInfo = new OrderPaymentModel { OrderNumber = 1234, CreditCardNumber = "1234567890123456", CreditCardSecurityCode = "123", Amount = 100};
+var paymentVerificationResponse = await paymentsClient.GetOrderPaymentAsync(orderPaymentInfo);
+PrintOut(paymentVerificationResponse);
+
+Console.ReadLine();
+
+var orderPaymentInfo2 = new OrderPaymentModel { OrderNumber = 1235, CreditCardNumber = "123456789012356", CreditCardSecurityCode = "123", Amount = 100 };
+var paymentVerificationResponse2 = await paymentsClient.GetOrderPaymentAsync(orderPaymentInfo2);
+PrintOut(paymentVerificationResponse2);
+
+Console.ReadLine();
+
+
+
+static void PrintOut(PaymentVerificationResponse paymentVerificationResponse)
+{
+    if (paymentVerificationResponse.Success == true)
+    {
+        Console.WriteLine($"Order Number {paymentVerificationResponse.OrderNumber}.{paymentVerificationResponse.Message} ");
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine($"Order Number {paymentVerificationResponse.OrderNumber}.{paymentVerificationResponse.Message}");
+        Console.WriteLine();
+    }
+}
